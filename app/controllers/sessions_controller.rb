5e4c10960e085a99c74d0c3edeeb2d62f65c0d78
David Maurer <davemaurermail@gmail.com>
2016-01-20 10:10:42 -0700

    add create for users and sessions. add route for new users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:notice] = "Login Successful. Welcome Back."
-      redirect_to links_path
+      redirect_to root_path
     else
       flash[:notice] = "Invalid login. Please try again."
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:notice] = "Sign Up Successful."
-        redirect_to links_path
-      else
-        flash[:notice] = "Something went wrong. Please try again."
-        render :new
-      end
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Sign Up Successful."
+      redirect_to root_path
+    else
+      flash[:notice] = "Something went wrong. Please try again."
+      render :new
     end
+  end
 
-    private
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
