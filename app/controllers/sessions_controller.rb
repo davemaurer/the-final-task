ac7505cf63efb7e4f296ffa49b8f0c023906a272
David Maurer <davemaurermail@gmail.com>
2016-01-20 09:59:06 -0700

    add controllers models and partials
    controllers for sessions and users, model for users, partial for messages so
    I can give notifications during actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Login Successful. Welcome Back."
+      redirect_to links_path
+    else
+      flash[:notice] = "Invalid login. Please try again."
+      render :new
+    end
+  end
+end
--- spec/features/users_can_log_in_and_out_spec.rb
+++ spec/features/users_can_log_in_and_out_spec.rb
@@ -6,4 +6,83 @@ RSpec.feature "User login/logout" do
 
     expect(page).to have_content('Sign Up Here')
   end
+
+  scenario "unregistered user creates an account" do
+    visit root_path
+
+    click_link('Sign Up Here')
+
+    expect(User.count).to eq(0)
+
+    expect(current_path).to eq('/users/new')
+    fill_in 'Email', with: 'newuser@newuser.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm Password', with: 'password'
+    click_on('Submit')
+
+    expect(User.count).to eq(1)
+    expect(User.first.email).to eq('newuser@newuser.com')
+    expect(current_path).to eq(links_path)
+    expect(page).to have_content('Hello, newuser@newuser.com')
+  end
+
+  scenario "unregistered user cannot use a duplicate email" do
+    visit root_path
+
+    click_link('Sign Up Here')
+
+    expect(User.count).to eq(0)
+
+    fill_in 'Email', with: 'newuser@newuser.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm Password', with: 'password'
+    click_on('Submit')
+
+    expect(User.count).to eq(1)
+
+    visit root_path
+
+    click_link('Sign Up Here')
+
+    fill_in 'Email', with: 'newuser@newuser.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm Password', with: 'password'
+    click_on('Submit')
+
+    expect(User.count).to eq(1)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Something went wrong. Please try again.')
+  end
+
+  scenario "user cannot leave password blank during sign up" do
+    visit root_path
+
+    click_link('Sign Up Here')
+
+    expect(User.count).to eq(0)
+
+    fill_in 'Email', with: 'newuser@newuser.com'
+    fill_in 'Password', with: ''
+    fill_in 'Confirm Password', with: ''
+    click_on('Submit')
+
+    expect(User.count).to eq(0)
+    expect(page).to have_content('Something went wrong. Please try again.')
+  end
+
+  scenario "user cannot leave email blank during sign up" do
+    visit root_path
+
+    click_link('Sign Up Here')
+
+    expect(User.count).to eq(0)
+
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirm Password', with: 'password'
+    click_on('Submit')
+
+    expect(User.count).to eq(0)
+    expect(page).to have_content('Something went wrong. Please try again.')
+  end
 end
